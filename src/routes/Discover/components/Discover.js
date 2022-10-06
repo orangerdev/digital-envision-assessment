50935a158672d00a81bda98589f3c5f9744f4246
Ridwan Arifandi <ridwan@99.co>
2022-10-06 08:41:09 +0700

    fix(data): set location on how to load data

--- src/routes/Discover/components/Discover.js
+++ src/routes/Discover/components/Discover.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import DiscoverBlock from './DiscoverBlock/components/DiscoverBlock';
 import '../styles/_discover.scss';
-import { getSpotifyFeaturedPlaylists, getSpotifyNewReleases, getSpotifyCategories } from '../../../helpers/spotify';
 
 export default class Discover extends Component {
   constructor() {
@@ -11,36 +10,34 @@ export default class Discover extends Component {
       newReleases: [],
       playlists: [],
       categories: [],
-      token: ""
     };
   }
 
   static getDerivedStateFromProps(props, state) {
-    return { token: props.token };
+    return { ...props }
   }
 
-  componentDidMount() {
-    Promise.all([
-      getSpotifyNewReleases(this.state.token),
-      getSpotifyFeaturedPlaylists(this.state.token),
-      getSpotifyCategories(this.state.token),
-    ]).then(([newReleases, playlists, categories]) => { 
-      this.setState((prevData) => {
-        return {
-          ...prevData,
-          newReleases: newReleases?.albums?.items ? newReleases.albums.items: [],
-          playlists: playlists?.playlists?.items ? playlists.playlists?.items: [],
-          categories: categories?.categories?.items ? categories.categories.items: [],
-        };
-      })
-    });
+  componentDidUpdate() {
+    // Promise.all([
+    //   getSpotifyNewReleases(this.state.token),
+    //   getSpotifyFeaturedPlaylists(this.state.token),
+    //   getSpotifyCategories(this.state.token),
+    // ]).then(([newReleases, playlists, categories]) => { 
+    //   this.setState((prevData) => {
+    //     return {
+    //       ...prevData,
+    //       newReleases: newReleases?.albums?.items ? newReleases.albums.items: [],
+    //       playlists: playlists?.playlists?.items ? playlists.playlists?.items: [],
+    //       categories: categories?.categories?.items ? categories.categories.items: [],
+    //     };
+    //   })
+    // });
   }
 
   render() {
-    const { newReleases, playlists, categories, favoritecolor } = this.state;
+    const { newReleases, playlists, categories } = this.state;
     return (
       <div className="discover">
-        {favoritecolor}
         <DiscoverBlock
           text="RELEASED THIS WEEK"
           id="released"
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { getSpotifyToken } from '../helpers/spotify';
 import Discover from './Discover';
+import { getSpotifyNewReleases, getSpotifyFeaturedPlaylists, getSpotifyCategories } from '../helpers/spotify';
 
 export default function Routes() {
-  const [token, setToken] = useState(null);
+  const [data, setData] = useState({newReleases: [], playlists: [], categories: []})
 
   useEffect(() => {
     let token = window.localStorage.getItem("token");
@@ -11,14 +12,22 @@ export default function Routes() {
     if (!token) {
       getSpotifyToken().then((token) => {
         window.localStorage.setItem("token", token);
-        setToken(token);
       });
-    } else {
-      setToken(token);
     }
 
-  }, [])
+    Promise.all([
+      getSpotifyNewReleases(token),
+      getSpotifyFeaturedPlaylists(token),
+      getSpotifyCategories(token),
+    ]).then(([newReleases, playlists, categories]) => {
+      setData({
+        newReleases: newReleases?.albums?.items ? newReleases.albums.items : [],
+        playlists: playlists?.playlists?.items ? playlists.playlists?.items : [],
+        categories: categories?.categories?.items ? categories.categories.items : [],
+      });
+    })
+  }, []);
   
   // Here you'd return an array of routes
-  return <Discover token={token} />;
+  return <Discover {...data} />;
 }
